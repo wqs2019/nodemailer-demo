8da5d88fca515dff07fe9c7761651a28bda98dee
wqs2019 <1196828446@qq.com>
2020-05-01 10:27:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
-let code = Math.random().toString().substr(2, 6);
+let code = Math.random().toString().substr(2, 6);//生成随机数验证码
 let transporter = nodemailer.createTransport({
   // host: 'smtp.ethereal.email',
   service: "qq", // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
@@ -11,14 +11,14 @@ let transporter = nodemailer.createTransport({
   secureConnection: true, // 使用了 SSL
   auth: {
     user: "alanwu.hd@qq.com",
-    pass: "nuhelngjweilgahb", //这是QQ的
-    //pass: "JPQFVHTYNPMVGPQX", //这是163的
+    pass: "xxxxxxxxxx", //这是QQ的授权码
+    //pass: "xxxxxxxxxxx", //这是163的授权码
   },
 });
 
 let mailOptions = {
   from: "alanwu.hd@qq.com", // sender address
-  to: "15768716486@163.com", // list of receivers
+  to: "157687xxxxxx@163.com", // list of receivers
   subject: "Hello,this is alan from China!", // Subject line
   // 发送text或者html格式
   text: `您的验证码是${code},验证码在10分钟内有效`, // plain text body
